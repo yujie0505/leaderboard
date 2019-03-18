6fb28f10fc6f99d8bac9484dfc10f70799f2c3fb
yujie <seven8355@gmail.com>
2019-03-18 11:11:19 +0800

    Refactor(app): add loadRanking()

--- app/app.vue
+++ app/app.vue
@@ -2,7 +2,31 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+
+  data () {
+    return {
+      ranking: []
+    }
+  },
+
+  created () {
+    this.loadRanking()
+  },
+
+  methods: {
+
+    loadRanking () {
+      const vm = this
+
+      axios.get(`https://testsite.staging.seekrtech.com/forest_rankings?n=20&last_pos=${vm.ranking.length}`)
+        .then(res => vm.ranking.push(...res.data.ranking))
+    }
+
+  }
+
 }
 </script>
 
